25fe47f57762a01697abf61ff1338bdd54c9c1f5
bangoo040993 <b40n50@gmail.com>
2023-09-15 21:39:42 -0400

    i have 3 test run i think ive might committed on the wrong page.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
-require('./config/database.js')
 const PORT = process.env.PORT || 9000;
 const app = require('./app-server')
 
+const mongoose = require('mongoose')
+
+
+mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB: This maybe our last chance - Tidus')
+})
 
 app.listen(PORT, () => {
 	console.log('I love you  ' + PORT + ' beyond')
